6ac4d7e03ff76e0b7243b0a4bad6f18bae73a550
Craig Vyvial <cp16net@gmail.com>
2016-06-01 11:16:32 -0500

    regenerate swagger code when make generate is called (#63)

--- scripts/generate-csm-client.sh
+++ scripts/generate-csm-client.sh
@@ -4,8 +4,8 @@
 
 if [ -d generated/CatalogServiceManager-client ]
 then
-	echo "${WARN_MAGENTA}==> Generated code found @ generated/CatalogServiceManager ${NO_COLOR}"
+	echo "${WARN_MAGENTA}==> Generated code found @ generated/CatalogServiceManager-client ${NO_COLOR}"
 else
-	echo "${OK_GREEN_COLOR}==> Calling swagger generate service @ generated/CatalogServiceManager ${NO_COLOR}"
+	echo "${OK_GREEN_COLOR}==> Calling swagger generate service @ generated/CatalogServiceManager-client ${NO_COLOR}"
 	swagger generate client -A CatlogServiceManager -t generated/CatalogServiceManager-client -f docs/swagger-spec/api.yml
 fi
