e32224b9f98d2c6ba74b73224454df1de9008507
carabasdaniel <daniel.carabas@hpe.com>
2016-06-07 06:05:26 +0300

    add status for dev-mssql

---
+++ csm-extensions/services/dev-mssql/cmd/status/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hpcloud/catalog-service-manager/csm-extensions/services/dev-mssql"
+	"github.com/hpcloud/catalog-service-manager/csm-extensions/services/dev-mssql/config"
+	"github.com/hpcloud/catalog-service-manager/csm-extensions/services/dev-mssql/provisioner"
+	"github.com/hpcloud/go-csm-lib/csm"
+	"github.com/pivotal-golang/lager"
+	"gopkg.in/caarlos0/env.v2"
+)
+
+func main() {
+
+	var logger = lager.NewLogger("mssql-extension")
+	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
+
+	conf := config.MssqlConfig{}
+	err := env.Parse(&conf)
+	if err != nil {
+		logger.Fatal("main", err)
+	}
+
+	if conf.Host == "" {
+		conf.Host = fmt.Sprintf("mssql.%s", conf.UcpDomainSuffix)
+	}
+
+	request, err := csm.GetCSMRequest(os.Args)
+	if err != nil {
+		logger.Fatal("main", err)
+	}
+
+	csmConnection := csm.NewCSMFileConnection(request.OutputPath, logger)
+	prov := provisioner.NewGoMssqlProvisioner(logger, conf)
+
+	extension := mssql.NewMSSQLExtension(prov, conf, logger)
+
+	response, err := extension.GetStatus()
+	if err != nil {
+		err := csmConnection.WriteError(err)
+		if err != nil {
+			logger.Fatal("main", err)
+		}
+		os.Exit(0)
+	}
+
+	err = csmConnection.Write(*response)
+	if err != nil {
+		logger.Fatal("main", err)
+	}
+}
