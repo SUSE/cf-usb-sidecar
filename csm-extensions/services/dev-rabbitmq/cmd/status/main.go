10d6344704d77ab390b232dd1b0d8760cdd2bb92
carabasdaniel <daniel.carabas@hpe.com>
2016-06-07 06:21:56 +0300

    add status implementation for dev-rabbitmq

---
+++ csm-extensions/services/dev-rabbitmq/cmd/status/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hpcloud/catalog-service-manager/csm-extensions/services/dev-rabbitmq"
+	"github.com/hpcloud/catalog-service-manager/csm-extensions/services/dev-rabbitmq/config"
+	"github.com/hpcloud/catalog-service-manager/csm-extensions/services/dev-rabbitmq/provisioner"
+	"github.com/hpcloud/go-csm-lib/csm"
+	"github.com/pivotal-golang/lager"
+	"gopkg.in/caarlos0/env.v2"
+)
+
+func main() {
+
+	var logger = lager.NewLogger("rabbitmq-extension")
+	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
+
+	conf := config.RabbitmqConfig{}
+	err := env.Parse(&conf)
+	if err != nil {
+		logger.Fatal("main", err)
+	}
+
+	if conf.DockerEndpoint == "" {
+		if conf.DockerHost == "" {
+			conf.DockerHost = fmt.Sprintf("rabbitmq.%s", conf.UcpDomainSuffix)
+		}
+
+		conf.DockerEndpoint = fmt.Sprintf("http://%s:%s", conf.DockerHost, conf.DockerPort)
+	}
+
+	request, err := csm.GetCSMRequest(os.Args)
+	if err != nil {
+		logger.Fatal("main", err)
+	}
+
+	csmConnection := csm.NewCSMFileConnection(request.OutputPath, logger)
+	prov := provisioner.NewRabbitHoleProvisioner(logger, conf)
+
+	extension := rabbitmq.NewRabbitmqExtension(prov, conf, logger)
+
+	response, err := extension.GetStatus()
+	if err != nil {
+		err := csmConnection.WriteError(err)
+		if err != nil {
+			logger.Fatal("main", err)
+		}
+		os.Exit(0)
+	}
+
+	err = csmConnection.Write(*response)
+	if err != nil {
+		logger.Fatal("main", err)
+	}
+}
