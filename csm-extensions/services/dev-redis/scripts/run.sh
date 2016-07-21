7f814d6502a4d374dd37266b95568f48c869d32c
carabasdaniel <daniel.carabas@hp.com>
2016-07-21 19:54:43 +0300

    Update redis start script (#98)
    * Update redis start script
    
    * Update start script host settings

--- csm-extensions/services/dev-redis/scripts/run.sh
+++ csm-extensions/services/dev-redis/scripts/run.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-if [ -z ${DOCKER_HOST} ]
+if [ -z "${DOCKER_HOST}" ]
 then
+  if [ -z "${REDIS_INT_SERVICE_PORT}" ]
+  then
+    export DOCKER_HOST="redis.${HCP_SERVICE_DOMAIN_SUFFIX}"
+  else
     export DOCKER_HOST="redis-int.${HCP_SERVICE_DOMAIN_SUFFIX}"
+  fi
+fi
+
+
+if [ -z "${DOCKER_PORT}" ]
+then
+    if [ -z "${REDIS_INT_SERVICE_PORT}" ]
+    then
+     export DOCKER_PORT=${REDIS_SERVICE_PORT}
+    else
+     export DOCKER_PORT=${REDIS_INT_SERVICE_PORT}
+    fi
 fi
 
 echo "Starting catalog-service-manager ..."
