d98cb408492e6a13dd2cb8af6f7adb84cf683c60
carabasdaniel <daniel.carabas@hp.com>
2016-07-21 19:54:57 +0300

    Update rabbitmq start script (#99)
    * Update rabbitmq start script
    
    * Update start script host settings

--- csm-extensions/services/dev-rabbitmq/scripts/run.sh
+++ csm-extensions/services/dev-rabbitmq/scripts/run.sh
@@ -2,7 +2,22 @@
 
 if [ -z ${DOCKER_HOST} ]
 then
-    export DOCKER_HOST="rabbitmq-int.${HCP_SERVICE_DOMAIN_SUFFIX}"
+  if [ -z "${RABBITMQ_INT_SERVICE_PORT}" ]
+  then
+   export DOCKER_HOST="rabbitmq.${HCP_SERVICE_DOMAIN_SUFFIX}"   
+  else
+   export DOCKER_HOST="rabbitmq-int.${HCP_SERVICE_DOMAIN_SUFFIX}"
+  fi
+fi
+
+if [ -z "${DOCKER_PORT}" ]
+then
+    if [ -z "${RABBITMQ_INT_SERVICE_PORT}" ]
+    then
+     export DOCKER_PORT=${RABBITMQ_SERVICE_PORT}
+    else
+     export DOCKER_PORT=${RABBITMQ_INT_SERVICE_PORT}
+    fi
 fi
 
 echo "Starting catalog-service-manager ..."
