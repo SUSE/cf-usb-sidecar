2949d23f6fcfb9b4a7a3513cb8ebfa69518c3d75
Saurabh Surana <saurabh.surana@hpe.com>
2016-11-03 08:47:20 -0700

    Added JdbcUrl to mysql config

--- csm-extensions/services/dev-mysql/config/binding.go
+++ csm-extensions/services/dev-mysql/config/binding.go
@@ -1,5 +1,7 @@
 package config
 
+import "fmt"
+
 type MySQLBinding struct {
 	Host     string `json:"host"`
 	Hostname string `json:"hostname"`
@@ -8,4 +10,11 @@ type MySQLBinding struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
 	Database string `json:"database"`
+	JdbcUrl  string `json:"jdbcUrl"`
+}
+
+var JdbcUrilTemplate = "jdbc:mysql://%[1]v:%[2]v/%[3]v?user=%[4]v&password=%[5]v"
+
+func GenerateConnectionString(input string, hostname string, port string, databaseName string, username string, password string) string {
+	return fmt.Sprintf(input, hostname, port, databaseName, username, password)
 }
