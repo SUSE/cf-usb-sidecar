3e10c1dea49bec3870ad45af490a634444db87d9
Andreas Kupries <andreas.kupries@suse.com>
2017-11-06 15:36:50 -0800

    Reworked swagger invokations to use the submoduled swagger via `go run`. Note that the new swagger expects the `-t` directory to exist, it will abort if not. The older 0.5 swagger silently created the missing directory. Fixed colored output.

--- scripts/generate-server.sh
+++ scripts/generate-server.sh
@@ -4,8 +4,20 @@
 
 if [ -d generated/CatalogServiceManager ]
 then
-	echo "${WARN_MAGENTA}==> Generated code found @ generated/CatalogServiceManager ${NO_COLOR}"
-else
-	echo "${OK_GREEN_COLOR}==> Calling swagger generate service @ generated/CatalogServiceManager ${NO_COLOR}"
-	swagger generate server -A CatlogServiceManager -t generated/CatalogServiceManager -f docs/swagger-spec/api.yml
+	printf "${WARN_MAGENTA}==> Generated code found @ generated/CatalogServiceManager ${NO_COLOR}"
+	exit 0
 fi
+
+printf "${OK_GREEN_COLOR}==> Calling swagger generate service @ generated/CatalogServiceManager ${NO_COLOR}\n"
+mkdir -p generated/CatalogServiceManager
+
+# Locate ourselves
+TOPDIR=$(cd "$(dirname "$0")/.." && pwd)
+
+# And run the sub-moduled swagger (which comes with a vendor matching go-openapi).
+GOPATH="${TOPDIR}:${GOPATH}" go run \
+    "${TOPDIR}"/src/github.com/go-swagger/go-swagger/cmd/swagger/swagger.go \
+    generate server \
+    -A CatlogServiceManager \
+    -t generated/CatalogServiceManager \
+    -f docs/swagger-spec/api.yml
