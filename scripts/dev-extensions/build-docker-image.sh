f8dcf6aa9c9a5259f43b1fae4b8cbb2278ff389f
Mark Yen <mark.yen@suse.com>
2018-01-16 14:42:14 -0800

    Helm-ize postgres dev service

--- scripts/dev-extensions/build-docker-image.sh
+++ scripts/dev-extensions/build-docker-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker build -t ${SIDECAR_EXTENSION_IMAGE_NAME}:${SIDECAR_EXTENSION_IMAGE_TAG} --rm -f Dockerfile       .
-if test -n "${SIDECAR_SETUP_IMAGE_NAME:-}" ; then
+if test -f Dockerfile-setup ; then
     docker build -t ${SIDECAR_SETUP_IMAGE_NAME}:${SIDECAR_SETUP_IMAGE_TAG}     --rm -f Dockerfile-setup .
 fi
