eaf594b25081114fe19c222e17b484e9303e52cf
Mark Yen <mark.yen@suse.com>
2018-01-16 18:06:42 -0800

    Switch to vendor/ instead of Godeps/_workspace/

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,10 @@ TOPDIR=$(cd "$(dirname "$0")/.." && pwd)
 TESTLIST=$(go list ./cmd/... ./src/... | grep -v github.com/go-swagger)
 
 # Manipulate the GOPATH to include both go-swagger and go-openapi from
-# the sub-moduled swagger for succesful compilation of test files.
-GOPATH="${TOPDIR}:${TOPDIR}/go-swagger:${GOPATH}" godep go test ${TESTLIST}
+# the sub-moduled swagger. Unfortunately, go(1) gets _really_ confused when
+# symlinks are involved; copying the tree is more likely to do the correct thing
+swaggerdir="$(mktemp -d)"
+trap "rm -rf '${swaggerdir}'" EXIT
+cp -r "${TOPDIR}/src/github.com/go-swagger/go-swagger/vendor" "${swaggerdir}/src"
+export GOPATH="${swaggerdir}:${GOPATH}"
+go test ${TESTLIST}
