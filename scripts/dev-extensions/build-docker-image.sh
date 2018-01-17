c39f4bc45764695197c2fc62e38a0b07c600123e
Mark Yen <mark.yen@suse.com>
2018-01-17 13:00:26 -0800

    extensions: exit on failure when building images
    Addresses:
    https://github.com/SUSE/cf-usb-sidecar/pull/3#pullrequestreview-89563208

--- scripts/dev-extensions/build-docker-image.sh
+++ scripts/dev-extensions/build-docker-image.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -o errexit
 docker build -t ${SIDECAR_EXTENSION_IMAGE_NAME}:${SIDECAR_EXTENSION_IMAGE_TAG} --rm -f Dockerfile       .
 if test -f Dockerfile-setup ; then
     docker build -t ${SIDECAR_SETUP_IMAGE_NAME}:${SIDECAR_SETUP_IMAGE_TAG}     --rm -f Dockerfile-setup .
