6b82ab7c0e2bd3830cfdf30c57461e68aea5855d
Mark Yen <mark.yen@suse.com>
2018-01-17 17:09:38 -0800

    release image: use multi-stage docker builds
    This simplifies the build to have one fewer intermediate image.  The
    build system could use further simplification.

--- scripts/docker/generate-build-image.sh
+++ scripts/docker/generate-build-image.sh
@@ -1,43 +1,42 @@
 #!/bin/sh
 
-. ${SIDECAR_ROOT}/scripts/colors.sh
+. "${SIDECAR_ROOT}/scripts/colors.sh"
 
-force_rebuild=$1
+force_rebuild="${1:-}"
 
-current_dir=$(pwd)
-script_dir=$(dirname "$0")
+current_dir="$(pwd)"
+script_dir="$(dirname "$0")"
 
-if ! [ -d ${current_dir}/.git ]
+if ! [ -e "${current_dir}/.git" ]
 then
-	printf "${ERROR_COLOR}==> Please execute script from catalog-service-manager's project root directory${NO_COLOR}\n"
-	exit 1	
+	printf "%b==> Please execute script from catalog-service-manager's project root directory%b\n" "${ERROR_COLOR}" "${NO_COLOR}"
+	exit 1
 fi
 
 if [ "${script_dir}" != "scripts/docker" ]
 then
-	printf "${ERROR_COLOR}==> Script directory is not correct, please run script from \${PROJECT_ROOT}/scripts/dockerv${NO_COLOR}\n"
+	printf "%b==> Script directory is not correct, please run script from \${PROJECT_ROOT}/scripts/dockerv%b\n" "${ERROR_COLOR}" "${NO_COLOR}"
 	exit 1	
 fi
 
-docker images | grep ${SIDECAR_BUILD_BASE_IMAGE_NAME} | grep ${SIDECAR_BUILD_BASE_IMAGE_TAG} > /dev/null 2>&1
-if [ $? -eq 0 ]
+if docker images | grep "${SIDECAR_BUILD_BASE_IMAGE_NAME}" | grep --quiet "${SIDECAR_BUILD_BASE_IMAGE_TAG}" > /dev/null 2>&1
 then
 	if [ "$force_rebuild" != "rebuild-image" ]
 	then
-		printf "${WARN_MAGENTA}==> ${SIDECAR_BUILD_BASE_IMAGE_NAME}:${SIDECAR_BUILD_BASE_IMAGE_TAG} image already exists!${NO_COLOR}\n"
+		printf "%b==> ${SIDECAR_BUILD_BASE_IMAGE_NAME}:${SIDECAR_BUILD_BASE_IMAGE_TAG} image already exists!%b\n" "${WARN_MAGENTA}" "${NO_COLOR}"
 		exit 0
 	fi
 	
-	if [ "$force_rebuild" == "rebuild-image" ]
+	if [ "$force_rebuild" = "rebuild-image" ]
 	then
-		printf "${OK_GREEN_COLOR}==> Removing old ${SIDECAR_BUILD_BASE_IMAGE_NAME}:${SIDECAR_BUILD_BASE_IMAGE_TAG} images ..${NO_COLOR}\n"
-		docker images | grep ${SIDECAR_BUILD_BASE_IMAGE_NAME} | grep ${SIDECAR_BUILD_BASE_IMAGE_TAG} | awk '{print $3}' | xargs -n 1 docker rmi -f > /dev/null 2>&1
+		printf "%b==> Removing old ${SIDECAR_BUILD_BASE_IMAGE_NAME}:${SIDECAR_BUILD_BASE_IMAGE_TAG} images ...%b\n" "${OK_GREEN_COLOR}" "${NO_COLOR}"
+		docker images | grep "${SIDECAR_BUILD_BASE_IMAGE_NAME}" | grep "${SIDECAR_BUILD_BASE_IMAGE_TAG}" | awk '{print $3}' | xargs -n 1 docker rmi -f > /dev/null 2>&1
 		sleep 5
 	fi	
 fi
 
-printf "${OK_GREEN_COLOR}==> Building ${SIDECAR_BUILD_BASE_IMAGE_NAME}:build image ..${NO_COLOR}\n"
+printf "%b==> Building ${SIDECAR_BUILD_BASE_IMAGE_NAME}:${SIDECAR_BUILD_BASE_IMAGE_TAG} image ...%b\n" "${OK_GREEN_COLOR}" "${NO_COLOR}"
 docker build \
-    --tag ${SIDECAR_BUILD_BASE_IMAGE_NAME}:${SIDECAR_BUILD_BASE_IMAGE_TAG} \
+    --tag "${SIDECAR_BUILD_BASE_IMAGE_NAME}:${SIDECAR_BUILD_BASE_IMAGE_TAG}" \
     --rm \
-    --file ${script_dir}/Dockerfile-build .
+    --file "${script_dir}/Dockerfile-build" .
--- scripts/docker/release/generate-release-base-image.sh
+++ scripts/docker/release/generate-release-base-image.sh
@@ -7,10 +7,10 @@ WARN_CYN_COLOR='\033[36m'
 ERROR_COLOR='\033[1;31m'
 NO_COLOR='\033[0m'
 
-force_rebuild=$1
+force_rebuild="${1:-}"
 
 . scripts/colors.sh
-scripts/docker/generate-build-image.sh $force_rebuild
+scripts/docker/generate-build-image.sh "${force_rebuild}"
 
 current_dir=$(pwd)
 script_dir=$(dirname "$0")
@@ -27,50 +27,10 @@ then
 	exit 1
 fi
 
-SIDECAR_BUILD_IMAGE_NAME=cf-usb-sidecar-build-binary
-SIDECAR_BUILD_IMAGE_TAG=latest
-SIDECAR_BUILD_CONTAINER_NAME=sidecar-build
+printf "${OK_GREEN_COLOR}==> Building ${SIDECAR_BASE_IMAGE_NAME}:${SIDECAR_BASE_IMAGE_TAG} image ..  ${NO_COLOR}\n"
+docker build -t ${SIDECAR_BASE_IMAGE_NAME}:${SIDECAR_BASE_IMAGE_TAG} --rm -f scripts/docker/release/Dockerfile-release .
 
-SIDECAR_BIN=${current_dir}/SIDECAR_BIN
-mkdir -p ${SIDECAR_BIN}
-rm -rf ${SIDECAR_BIN}/catalog-service-manager
+echo ""
+echo ""
 
-docker images | grep ${SIDECAR_BUILD_IMAGE_NAME} | grep ${SIDECAR_BUILD_IMAGE_TAG} > /dev/null 2>&1
-if [ $? -eq 0 ]
-then
-	printf "${OK_GREEN_COLOR}==> Remove ${SIDECAR_BUILD_IMAGE_NAME}:${SIDECAR_BUILD_IMAGE_TAG} image from docker before building new image ${NO_COLOR}\n"
-	docker rmi ${SIDECAR_BUILD_IMAGE_NAME}:${SIDECAR_BUILD_IMAGE_TAG}
-fi
-
-printf "${OK_BG_COLOR}==> Building ${SIDECAR_BUILD_IMAGE_NAME}:${SIDECAR_BUILD_IMAGE_TAG} image (to build catalog-service-manager binary).. ${NO_COLOR}\n"
-
-docker build -t ${SIDECAR_BUILD_IMAGE_NAME}:${SIDECAR_BUILD_IMAGE_TAG} --rm -f scripts/docker/release/Dockerfile-release-build .
-
-docker images | grep ${SIDECAR_BUILD_IMAGE_NAME} | grep ${SIDECAR_BUILD_IMAGE_TAG} > /dev/null 2>&1
-if [ $? -eq 0 ]
-then
-	printf "${OK_GREEN_COLOR}==> Copying CMS binary to the host ${NO_COLOR}\n"
-	docker run --name ${SIDECAR_BUILD_CONTAINER_NAME} -v ${SIDECAR_BIN}:/catalog-service-manager-bin/ ${SIDECAR_BUILD_IMAGE_NAME}:${SIDECAR_BUILD_IMAGE_TAG}
-fi
-
-printf "${OK_GREEN_COLOR}==> Removing ${SIDECAR_BUILD_IMAGE_NAME} container  ${NO_COLOR}\n"
-docker ps -a | grep ${SIDECAR_BUILD_CONTAINER_NAME} | awk '{print $1}' | xargs -n 1  docker rm
-
-
-if [ -f ${SIDECAR_BIN}/catalog-service-manager ]
-then
-	printf "${OK_BG_COLOR}==> CSM binary is built successfully ${NO_COLOR}\n"
-	echo ""
-	printf "${OK_GREEN_COLOR}==> Removing ${SIDECAR_BASE_IMAGE_NAME}:${SIDECAR_BASE_IMAGE_TAG} images  ${NO_COLOR}\n"
-	docker images | grep ${SIDECAR_BASE_IMAGE_NAME} | grep -v ${SIDECAR_BUILD_BASE_IMAGE_NAME}  | grep ${SIDECAR_BASE_IMAGE_TAG} | awk '{print $3}' | xargs -n 1 docker rmi -f
-
-	sleep 5
-	echo ""
-	printf "${OK_GREEN_COLOR}==> Building ${SIDECAR_BASE_IMAGE_NAME}:${SIDECAR_BASE_IMAGE_TAG} image ..  ${NO_COLOR}\n"
-	docker build -t ${SIDECAR_BASE_IMAGE_NAME}:${SIDECAR_BASE_IMAGE_TAG} --rm -f scripts/docker/release/Dockerfile-release .
-
-	echo ""
-	echo ""
-
-	printf "${OK_BG_COLOR}==> ${SIDECAR_BASE_IMAGE_NAME}:${SIDECAR_BASE_IMAGE_TAG} built successfully ${NO_COLOR}\n"
-fi
+printf "${OK_BG_COLOR}==> ${SIDECAR_BASE_IMAGE_NAME}:${SIDECAR_BASE_IMAGE_TAG} built successfully ${NO_COLOR}\n"
