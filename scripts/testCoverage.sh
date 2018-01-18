05f401ae452aac36e5aad00c2b33cc9fb97d6889
Mark Yen <mark.yen@suse.com>
2018-01-17 17:18:03 -0800

    testCoverage: Fix for change in vendoring

--- scripts/testCoverage.sh
+++ scripts/testCoverage.sh
@@ -1,32 +1,38 @@
 #!/bin/sh
 
-set -e
+set -o errexit
+set -o nounset
 
 TOPDIR=$(cd "$(dirname "$0")/.." && pwd)
 
 workdir=.cover
-profile="$workdir/cover.out"
+profile="${workdir}/cover.out"
 mode=count
 
 # clean up the artifacts for coverage.
-rm -rf $workdir
+rm -rf "${workdir}"
+
+# Manipulate the GOPATH to include both go-swagger and go-openapi from
+# the sub-moduled swagger. Unfortunately, go(1) gets _really_ confused when
+# symlinks are involved; copying the tree is more likely to do the correct thing
+swaggerdir="$(mktemp -d)"
+trap "rm -rf '${swaggerdir}'" EXIT
+cp -r "${TOPDIR}/src/github.com/go-swagger/go-swagger/vendor" "${swaggerdir}/src"
+export GOPATH="${swaggerdir}:${GOPATH}"
 
 generate_cover_data() {
-    mkdir -p "$workdir"
+    mkdir -p "${workdir}"
 
     for pkg in "$@"; do
-		echo $pkg
-        f="$workdir/$(echo $pkg | tr / -).cover"
-		echo $f
-        GOPATH="${TOPDIR}:${TOPDIR}/go-swagger:${GOPATH}" godep go test \
-	    -covermode="$mode" \
-	    -coverprofile="$f" "$pkg"
+        f="${workdir}/$(echo "${pkg}" | tr / -).cover"
+        printf "%s => %s\n" "${pkg}" "${f}"
+        go test -covermode="${mode}" -coverprofile="${f}" "${pkg}"
     done
 
-    echo "mode: $mode" >"$profile"
-    grep -h -v "^mode:" "$workdir"/*.cover >>"$profile"
+    echo "mode: ${mode}" >"${profile}"
+    grep -h -v "^mode:" "${workdir}"/*.cover >>"${profile}"
 }
 
 generate_cover_data $(go list ./cmd/... ./src/... | grep -v github.com/go-swagger)
-go tool cover -func="$profile"
-go tool cover -html="$profile"
+go tool cover -func="${profile}"
+go tool cover -html="${profile}"
