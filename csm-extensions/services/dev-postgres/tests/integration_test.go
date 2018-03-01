9dc2f1910fe86c68ec58f956d504f9397fa2838b
Alex Harford <alex.harford@suse.com>
2018-03-01 14:48:42 -0800

    Update error message for swagger generated code
    The new swagger generated code doesn't allow access to the detailed
    error message.

--- csm-extensions/services/dev-postgres/tests/integration_test.go
+++ csm-extensions/services/dev-postgres/tests/integration_test.go
@@ -124,7 +124,7 @@ func TestDeleteWorkspaceShouldFail(t *testing.T) {
 		t.Logf("Delete workspace resp: %s", resp.Error())
 	}
 	if assert.Error(err, "There should be an error while deleting a non existing workspace") {
-		assert.Contains(err.Error(), "pq: database \"dtestonnllyy123\" does not exist", "Incorrect answer when deleting a database that does not exist")
+		assert.Contains(err.Error(), "[DELETE /workspaces/{workspace_id}][500] deleteWorkspace default", "Incorrect answer when deleting a database that does not exist")
 	}
 	assert.Nil(resp, "There should be no correct unswer when deleting a non existing workspace")
 }
@@ -144,7 +144,7 @@ func TestGetWorkspaceShouldFail(t *testing.T) {
 		t.Logf("Get workspace resp: Status = %s, ProcessingType = %s", resp.Payload.Status, resp.Payload.ProcessingType)
 	}
 	if assert.Error(err, "Expected error since workspace does not exist") {
-		assert.Contains(err.Error(), "Workspace does not exist", "The error message is incorrect for getting an inexistent workspace")
+		assert.Contains(err.Error(), "[GET /workspaces/{workspace_id}][404] getWorkspace default", "The error message is incorrect for getting an inexistent workspace")
 	}
 	assert.Nil(resp, "response should be nil since there was an error")
 }
@@ -296,7 +296,7 @@ func TestCreateWorkspaceShouldFail(t *testing.T) {
 		t.Logf("Create workspace resp: Status = %s, ProcessingType = %s", resp.Payload.Status, resp.Payload.ProcessingType)
 	}
 	if assert.Error(err, "There should be an error when creating an workspace that already exists") {
-		assert.Contains(err.Error(), "pq: database \"dtestonnllyy123\" already exists", "There should be an error message stating that this db already exists when attempting to create an existing database")
+		assert.Contains(err.Error(), "[POST /workspaces][500] createWorkspace default", "There should be an error message stating that this db already exists when attempting to create an existing database")
 	}
 	assert.Nil(resp, "There should be no correct unswer when creating a workspace that already exists")
 
